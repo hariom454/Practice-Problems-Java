30a6bb844940dd05c538449a346da7a879f916e5
hariom454 <thakurhariomsingh498@gmail.com>
2024-06-14 09:34:51 +0530

    compare

--- src/main/com/shashtra/generics/OrderedPair.java
+++ src/main/com/shashtra/generics/OrderedPair.java
@@ -25,5 +25,17 @@ public class OrderedPair<K, V> implements Pair<K, V> {
     OrderedPair<String, String> p2 = new OrderedPair<>("myName", "Hariom Singh");
     System.out.println("content of first pair: " + p1.getKey() + " " + p1.getValue());
     System.out.println("Content of second pair: " + p2.getKey() + " " + p2.getValue());
+
+    OrderedPair<String, Integer> p3 = new OrderedPair<>("myRollNo", 1);
+    boolean isSame = Util.compare(p1, p3);
+    System.out.println("P1 and P3 are: " + (isSame ? "same" : "different"));
+
+  }
+}
+
+class Util {
+
+  public static <K, V> boolean compare(OrderedPair<K, V> p1, OrderedPair<K, V> p2) {
+    return p1.getKey().equals(p2.getKey()) && p1.getValue().equals(p2.getValue());
   }
 }
