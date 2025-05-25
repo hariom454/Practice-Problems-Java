35db8ff466bd6229fafa03e93b373079600c4acb
hariom454 <thakurhariomsingh498@gmail.com>
2025-05-25 17:19:48 +0530

    simple producer consumer implementation using synchronized block and lock.

---
+++ src/main/com/shashtra/concurrency/ProducerConsumerExample.java
@@ -0,0 +1,73 @@
+package com.shashtra.concurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ProducerConsumerExample {
+
+  private static final List<Integer> buffer = new ArrayList<>();
+  private static final int CAPACITY = 10;
+  private static final Object lock = new Object();
+
+  static void producer() throws InterruptedException {
+    synchronized (lock) {
+      System.out.println("buffer producer size: " + buffer.size());
+      while (buffer.size() == CAPACITY) {
+        System.out.println("Buffer is full, waiting for it to get empty!");
+        lock.wait();
+      }
+      int item = new Random().nextInt(100);
+      System.out.println("adding item: " + item);
+      buffer.add(item);
+      lock.notifyAll();
+    }
+  }
+
+  static void consumer() throws InterruptedException {
+    synchronized (lock) {
+      System.out.println("Buffer consumer size: " + buffer.size());
+      while (buffer.isEmpty()) {
+        System.out.println("Buffer is empty! waiting for items");
+        lock.wait();
+      }
+      Integer item = buffer.removeLast();
+      System.out.println("Remove item: " + item);
+      lock.notifyAll();
+    }
+
+  }
+
+
+  public static void main(String[] args) throws InterruptedException {
+
+    try (ExecutorService executors = Executors.newVirtualThreadPerTaskExecutor()) {
+      List<Future<?>> futures = new ArrayList<>();
+      for (int i = 0; i < 100; i++) {
+        executors.submit(() -> {
+          try {
+            producer();
+          } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+          }
+        });
+
+        executors.submit(() -> {
+          try {
+            consumer();
+          } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+          }
+        });
+
+      }
+
+    }
+
+
+  }
+
+}
